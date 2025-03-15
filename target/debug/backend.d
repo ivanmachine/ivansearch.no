b2e5b9ff0e21eb26109eea59595b3de13bee7756
Ivan Tarnyagin <ivan.tarnyagin@gmail.com>
2025-03-15 05:28:48 +0100

    Started creating the base structure of the project, and will try deploying it now - 99% chance it will fail

--- target/debug/backend.d
+++
@@ -1,1 +0,0 @@
-/Users/ivan/projects/ivansearch.no/target/debug/backend: /Users/ivan/projects/ivansearch.no/backend/src/main.rs
--- target/debug/deps/backend-9f6368b765768f75.d
+++
@@ -1,5 +0,0 @@
-/Users/ivan/projects/ivansearch.no/target/debug/deps/backend-9f6368b765768f75: backend/src/main.rs
-
-/Users/ivan/projects/ivansearch.no/target/debug/deps/backend-9f6368b765768f75.d: backend/src/main.rs
-
-backend/src/main.rs:
