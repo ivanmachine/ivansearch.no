9a49f9e04fbd6fe6ae663d3bf9b9b5e35a473528
Ivan Tarnyagin <ivan.tarnyagin@gmail.com>
2025-03-15 10:05:28 +0100

    Added Linux linker & testin again

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
-    println!("Starting server on 0.0.0.0:8000");
+    println!("Starting server on 0.0.0.0:3000");
 
     // Start the server on all interfaces (0.0.0.0)
     warp::serve(hello)
